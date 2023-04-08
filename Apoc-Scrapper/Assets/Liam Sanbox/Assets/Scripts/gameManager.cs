41c28cc426444ddb2b72441ae59ae89ce8f9dfcd
lhans1 <lhans@student.fullsail.edu>
2023-04-08 18:39:57 -0400

    Formatted my Sandbox folder, got player movement working in my scene
    Needed to add an empty object and and add the gameManager script as a component and then attach our player and playerController script to that object

---
+++ Apoc-Scrapper/Assets/Liam Sanbox/Assets/Scripts/gameManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [Header("----- Player Stuff -----")]
+    public GameObject player;
+    public playerController playerScript;
+
+    [Header("----- UI Stuff -----")]
+    public GameObject activeMenu;
+    public GameObject pauseMenu;
+    public GameObject winMenu;
+    public GameObject loseMenu;
+    public Image HPBar;
+    public TextMeshProUGUI enemiesRemainingText;
+
+    public int enemiesRemaining;
+
+    public bool isPaused;
+    float timeScaleOriginal;
+
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+        timeScaleOriginal = Time.timeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && activeMenu == null)
+        {
+            isPaused = !isPaused;
+            activeMenu = pauseMenu;
+            pauseMenu.SetActive(isPaused);
+
+            if (isPaused)
+            {
+                PauseState();
+            }
+            else
+            {
+                UnpauseState();
+            }
+        }
+
+    }
+
+    public void PauseState()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void UnpauseState()
+    {
+        Time.timeScale = timeScaleOriginal;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        activeMenu.SetActive(false);
+        activeMenu = null;
+    }
+
+    public void UpdateGameGoal(int amount)
+    {
+        enemiesRemaining += amount;
+        enemiesRemainingText.text = enemiesRemaining.ToString("F0");
+
+        if(enemiesRemaining <= 0)
+        {
+            activeMenu = winMenu;
+            activeMenu.SetActive(true);
+            PauseState();
+        }
+    }
+
+    public void PlayerDead()
+    {
+        PauseState();
+        activeMenu = loseMenu;
+        activeMenu.SetActive(true);
+    }
+}
