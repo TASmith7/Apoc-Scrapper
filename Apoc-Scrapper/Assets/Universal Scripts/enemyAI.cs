4952c472988e3842710379a6127d6ebee27f2690
DaJagerBomb1 <106552624+DaJagerBomb1@users.noreply.github.com>
2023-04-09 12:44:02 -0500

    TurretAi
    Turret ai tracks player using EnemyAi script now. TurretAi has been deleted.  turret still drops to the floor when game is going. will work on this.

--- Apoc-Scrapper/Assets/Universal Scripts/enemyAI.cs
+++ Apoc-Scrapper/Assets/Universal Scripts/enemyAI.cs
@@ -69,17 +69,17 @@ public class enemyAI : MonoBehaviour, IDamage
             // if the object we are hitting is the player, AND the angle to our player is within our sight angle
             if(hit.collider.CompareTag("Player") && angleToPlayer <= sightAngle)
             {
-                // if we can see the player, remember what the original stopping distance was, and reset it to that
-                agent.stoppingDistance = stoppingDistanceOrig;
+                
+                
 
                 // this gets the enemy to move in the direction of our player
-                agent.SetDestination(gameManager.instance.player.transform.position);
+                //agent.SetDestination(gameManager.instance.player.transform.position);
 
-                // if the agent has reached their stopping distance (i.e. the player is within the stopping distance we set), call face player method
-                if (agent.remainingDistance <= agent.stoppingDistance)
-                {
+               
+                
+                
                     FacePlayer();
-                }
+                
 
                 if (!isShooting)
                 {
